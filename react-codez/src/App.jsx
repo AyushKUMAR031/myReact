4beaac36023b226c92ce32b50f047a9ac15f7f5c
AyushKUMAR031 <ayushkumar03192003@gmail.com>
2024-06-09 14:13:37 +0530

    updating codes

--- react-codez/src/App.jsx
+++ react-codez/src/App.jsx
@@ -3,15 +3,21 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Accordian from './components/accordian/acc';
 import RandomColor from './components/random-color/color';
+import StarRating from './components/star-rating/star';
 
 function App() {
   return (
     <div className="App">
+      
+      {/*component of accordian*/}
       <Accordian />
-
+        
+      {/*component of random color*/}
       <RandomColor />
 
-      starRating
+      {/*component of star rating*/}
+      <StarRating />
+      
     </div>
   );
 }
--- react-codez/src/components/star-rating/star.jsx
+++ react-codez/src/components/star-rating/star.jsx
@@ -1,40 +1,42 @@
 import { useState } from 'react';
 import { FaStar } from 'react-icons/fa';
+import './starStyle.css';
 
-export default function starRating({ noOfStar = 5 }) {
+export default function StarRating({ noOfStar = 5 }) {
     const [rating, setRating] = useState(0);
     const [hover, setHover] = useState(0);
 
-    function handleClick(getCurrentIndex) {
-        console.log(getCurrentIndex);
+    function handleClick(currentIndex) {
+        setRating(currentIndex);
     }
 
-    function handleMouseEnter(getCurrentIndex) {
-        console.log(getCurrentIndex);
-
+    function handleMouseEnter(currentIndex) {
+        setHover(currentIndex);
     }
 
-    function handleMouseLeave(getCurrentIndex) {
-        console.log(getCurrentIndex);
+    function handleMouseLeave() {
+        setHover(rating);
 
     }
 
     return (
-        <div className="star-Rating">
+        <div className={'starRating'} >
             <h1>Star Rating</h1>
-            {
-                [...Array(noOfStar)].map((_, index) => {
-                    index += 1;
+            {[...Array(noOfStar)].map((_, index) => {
+                index++;
 
-                    return <FaStar
+                return (
+                    <FaStar
                         key={index}
+                        className={index <= (hover || rating) ? 'active' : 'inactive'}
                         onClick={() => handleClick(index)}
-                        onMouseEnter={() => handleMouseEnter(index)}
-                        onMouseLeave={() => handleMouseLeave(index)}
+                        onMouseMove={() => handleMouseEnter(index)}
+                        onMouseLeave={() => handleMouseLeave()}
                         size={40}
-                    />
-                })
+                    ></FaStar>
+                );
+            })
             }
         </div>
-    )
+    );
 }
